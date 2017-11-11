7399a1dd9187fe4aab9b7649d3054aa091f205f9
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-11-11 16:59:23 +0600

    +

--- polyndrome.js
+++ polyndrome.js
@@ -1,10 +1,8 @@
 function polyndrome(str) {
-	str = str.toUpperCase()
-		.replace(/\./g, '')
-		.replace(/\s/g, '')
-		.replace(/\!/g, '')
-		.replace(/\?/g, '')
-		.replace(/\,/g, '');
+	if (typeof str !== 'string') return 'Arg is not a string type';
+	str = str
+		.toUpperCase()
+		.replace(/[^a-z0–9]/ig, '');
 	var half = ~~(str.split('').length / 2)
 	var left = '';
 	var right = [];
@@ -14,7 +12,7 @@ function polyndrome(str) {
 		right.unshift(str[i]);
 	for (var i = 0; i < left.length; i++)
 		if (right.join('')[i] !== left[i]) return false;
-	
+
 	console.log(left, right.join(''))
 	return true;
 }
@@ -24,3 +22,5 @@ function polyndrome(str) {
 console.log(polyndrome('1234321'))
 console.log(polyndrome('nomelonnolemon'));
 console.log(polyndrome('A man, a plan, a canal, Panama!'));
+console.log(polyndrome(32423));
+console.log(polyndrome());
