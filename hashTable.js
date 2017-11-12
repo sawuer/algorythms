ae3c6f17138e481eca56a4fe150b3359f84160a9
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-11-12 12:50:46 +0600

    +

--- hashTable.js
+++ hashTable.js
@@ -1,30 +1,44 @@
-var hashTable = {};
+var usersAges = {};
 
 
-hashTable['Sowyer'] = 22;
-hashTable['Ramona'] = 26;
-hashTable['Linker'] = 22;
+usersAges['Sowyer'] = 22;
+usersAges['Ramona'] = 26;
+usersAges['Linker'] = 22;
 
 function putInHashTable(table, key, val) {
-	return table[key] = val;
+	if (table.hasOwnProperty(key)) {
+		return console.log('This key already exists');
+	}
+	table[key] = val;
 };
 
 function deleteInHashTable(table, key) {
-	return delete table[key];
+	delete table[key];
 };
 
-function printInHashTable(table) {
+function printHashTable(table) {
 	Object.keys(table).forEach(i => {
-		if (typeof table[i] !== 'function')
+		if (typeof table[i] !== 'function') {
 			console.log(i, table[i]);
+		}
 	});
 	console.log('')
 };
 
+function getHashTableKey(table, key) {
+	console.log(table[key]);
+}
 
 
-printInHashTable(hashTable);
-putInHashTable(hashTable, 'Follow', 43);
-printInHashTable(hashTable);
-deleteInHashTable(hashTable, 'Sowyer');
-printInHashTable(hashTable);
+
+printHashTable(usersAges);
+putInHashTable(usersAges, 'Follow', 43);
+printHashTable(usersAges);
+deleteInHashTable(usersAges, 'Sowyer');
+printHashTable(usersAges);
+deleteInHashTable(usersAges, 'Follow');
+printHashTable(usersAges);
+putInHashTable(usersAges, 'Follow', 433);
+printHashTable(usersAges);
+getHashTableKey(usersAges, 'Follow');
+
