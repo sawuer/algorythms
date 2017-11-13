202ab06cf697cf46953a0c46a4211841d77bdbdf
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-11-14 00:27:04 +0600

    stack

---
+++ stacks.js
@@ -0,0 +1,44 @@
+// functions: push, pop, peek, length
+
+var Stack = (function() {
+
+	function constructor() {
+		this.count = 0;
+		this.storage = {};
+	}
+
+	constructor.prototype = {
+		push(val) {
+			this.storage[this.count] = val;
+			this.count++;
+		},
+		pop() {
+			if (this.count === 0) return undefined;
+			this.count--;
+			var res = this.storage[this.count];
+			delete this.storage[this.count];
+			return res;
+		},
+		size() {
+			return this.count;
+		},
+		peek() {
+			return this.storage[this.count-1];
+		}
+	}
+
+	return constructor;
+
+})();
+
+
+var names = new Stack();
+
+
+names.push('Sowyer');
+names.push('Ramona');
+names.push('Linker');
+console.log(names);
+names.pop();
+// names.peek();
+console.log(names);
