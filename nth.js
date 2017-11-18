ae73dcde624ac3938e144a62c566b9addb6b14d6
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-11-19 01:08:40 +0600

    +

--- nth.js
+++ nth.js
@@ -1,14 +1,23 @@
-function arrayToList(arr) {
-
+function arrayToList(arr, hist={}, idx=0) {
+	if (idx < arr.length) {
+		hist['value'] = arr[idx];
+		hist['rest'] = {}; 
+		arrayToList(arr, hist.rest,  ++idx)
+	}
+	return hist;
 }
 
 
-console.log(arrayToList([10, 20, 30, 40, 50])); 
+console.log(arrayToList([10, 20, 30, 40])); 
 // { 
-// 	value: 10, rest: {
-// 		value: 20, rest: {
-// 			value: 30, rest: {
-// 				value: 40, rest: null
+// 	value: 10, 
+//	rest: {
+// 		value: 20, 
+//		rest: {
+// 			value: 30, 
+//			rest: {
+// 				value: 40, 
+//				rest: null
 // 			}
 // 		}
 // 	}
